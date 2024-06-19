b3db6389033715b4701e3cf247dc9f5c2a6977b1
choidoyeong <doyeong1209@gmail.com>
2024-06-19 19:32:43 +0900

    repository 추가

--- src/main/kotlin/com/example/urlshortener/repositories/UrlShortenerRepository.kt
+++ src/main/kotlin/com/example/urlshortener/repositories/UrlShortenerRepository.kt
@@ -1,4 +1,8 @@
 package com.example.urlshortener.repositories
 
-class UrlShortenerRepository {
-}
+import com.example.urlshortener.models.UrlShortener
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UrlShortenerRepository : CrudRepository<UrlShortener, Long> {}
