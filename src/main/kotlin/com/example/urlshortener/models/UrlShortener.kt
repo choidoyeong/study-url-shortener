4f47e0a24b2e5a2bd3afa2114410e6b2ac7d2caf
choidoyeong <doyeong1209@gmail.com>
2024-06-19 12:56:59 +0900

    init

---
+++ src/main/kotlin/com/example/urlshortener/models/UrlShortener.kt
@@ -0,0 +1,5 @@
+package com.example.urlshortener.models
+
+import org.springframework.data.annotation.Id
+
+data class UrlShortener(@Id val id: Long = 0,)
---
+++ src/main/kotlin/com/example/urlshortener/repositories/UrlShortenerRepository.kt
@@ -0,0 +1,4 @@
+package com.example.urlshortener.repositories
+
+class UrlShortenerRepository {
+}
