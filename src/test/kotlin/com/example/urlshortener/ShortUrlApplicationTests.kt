5ebca350fda2f6f9ccad0342b632a0f6c3bd9039
choidoyeong <doyeong1209@gmail.com>
2024-06-20 13:07:00 +0900

    post /short-links 테스트 추가

--- src/test/kotlin/com/example/urlshortener/ShortUrlApplicationTests.kt
+++ src/test/kotlin/com/example/urlshortener/ShortUrlApplicationTests.kt
@@ -1,13 +1,31 @@
 package com.example.urlshortener
 
+import com.example.urlshortener.controllers.requests.CreateShortUrlRequest
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
+@AutoConfigureMockMvc
 @SpringBootTest
-class ShortUrlApplicationTests {
+class ShortUrlApplicationTests (@Autowired private val mockMvc: MockMvc) {
 
     @Test
-    fun contextLoads() {
+    fun createShortUrl() {
+        val requestBody = jacksonObjectMapper().writeValueAsString(CreateShortUrlRequest(url = "http://test.com"))
+        val action= mockMvc.perform(MockMvcRequestBuilders.post("/short-links")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(requestBody)
+        )
+        action.andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.url").value("http://test.com"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.shortUrlId").value("MQ=="))
     }
 
 }
